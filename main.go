0779dc269bb8523bc8bfca1a9f6296b20856a7a6
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2025-04-25 11:48:58 -0400

    Learned usage of arrays and slicing in Go

--- main.go
+++ main.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	str := "Hello World!"
-	fmt.Println(str)
+
+	names := [4]string{"Abe", "Absol", "Akhenatun", "Aaron"}
+	scores := []int{22, 45, 66}
+	fmt.Println(scores[1])
+
+	fmt.Println(names, len(names))
+
+	scores[1] = 55
+	fmt.Println(scores[1])
+	scores = append(scores, 21)
+	fmt.Println(scores, len(scores))
+	rangeOne := names[1:3]
+	fmt.Print(rangeOne)
 }
