e133675064f8db7f7f7ecd8307459dc5aaf7c5bf
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2025-04-25 12:04:21 -0400

    learned usage of looping in Go

--- main.go
+++ main.go
@@ -5,15 +5,7 @@ import "fmt"
 func main() {
 
 	names := [4]string{"Abe", "Absol", "Akhenatun", "Aaron"}
-	scores := []int{22, 45, 66}
-	fmt.Println(scores[1])
-
-	fmt.Println(names, len(names))
-
-	scores[1] = 55
-	fmt.Println(scores[1])
-	scores = append(scores, 21)
-	fmt.Println(scores, len(scores))
-	rangeOne := names[1:3]
-	fmt.Print(rangeOne)
+	for index, value := range names {
+		fmt.Println("Pos is:", index, "value is:", value)
+	}
 }
