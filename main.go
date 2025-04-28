c8701acf6e96811fbd7b5b12fb28f63b92669d99
Muhammad Moin Uddin <98498075+mmoinsiaam@users.noreply.github.com>
2025-04-27 20:06:17 -0400

    Added helper function getInput() and a function for prompts

--- main.go
+++ main.go
@@ -7,20 +7,33 @@ import (
 	"strings"
 )
 
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
 // takes in user input to create bill
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Enter bill name: ")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	name, _ := getInput("Create new bill - ", reader)
 	b := newBill(name)
 	fmt.Println("Created the bill - ", b.name)
 	return b
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add an item, s - save the bill, t - add tip): ", reader)
+	fmt.Println(opt)
+}
+
 func main() {
 	mybill := createBill()
+	promptOptions(mybill)
 
 	fmt.Println(mybill)
 }
